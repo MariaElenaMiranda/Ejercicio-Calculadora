5dddab3c283f00ad51e3a46d9e5719e96f9748bf
María Elena Miranda <mara.mirandav@gmail.com>
2025-06-06 18:36:57 +0200

    Actualización en el menu principal: Se crea funcion lista_numeros, que reemplaza a convertir_numeros

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 from os import system
 from calculadora import Calculadora
+from math import trunc
 
 #Función para crear el menu
 def menu() -> None:
@@ -7,39 +8,49 @@ def menu() -> None:
     print("¡Bienvenido/a a la app calculadora!\n")
     print("Instrucciones:\n" +
         "- Selecciona la operación que deseas realizar\n" +
-        "- Ingresa los números a operar separados por una coma\n")
+        "- Ingresa los números a operar\n")
 
     opciones = ["1.- Sumar", "2.- Restar", "3.- Multiplicar", "4.- Dividir", "5.- Salir"]
     for opcion in opciones:
         print(opcion)
 
-#Función para convertir los numeros (str) en una lista de numeros (int | float)
-def convertir_numero(texto_numeros_str:str) -> list[int | float]:
-    lista_numeros_str = texto_numeros_str.split(",")
-    lista_numeros_final = []
-    for num in lista_numeros_str:
-        numero_limpio = num.strip()
-        try:
-            numero = float(numero_limpio)
-            if numero.is_integer():
-                numero = int(numero)
-            lista_numeros_final.append(numero)
-        except ValueError:
-            raise ValueError(f"Valor inválido: {numero_limpio}")
-    return lista_numeros_final
+# #Función para convertir los numeros (str) en una lista de numeros (int | float)
+# def convertir_numero(texto_numeros_str:str) -> list[int | float]:
+#     lista_numeros_str = texto_numeros_str.split(",")
+#     lista_numeros_final = []
+#     for num in lista_numeros_str:
+#         numero_limpio = num.strip()
+#         try:
+#             numero = float(numero_limpio)
+#             if numero.is_integer():
+#                 numero = int(numero)
+#             lista_numeros_final.append(numero)
+#         except ValueError:
+#             raise ValueError(f"Valor inválido: {numero_limpio}")
+#     return lista_numeros_final
 
+def lista_numeros() -> list[int | float]:
+    lista = []
+    while True:
+        try:
+            numero = float(input('Ingresa un número: '))
+            if numero:
+                lista.append(numero)
+        except ValueError:
+            print('Ingresa solo números enteros o decimales')
+        otro = input('Enter para continuar, "salir" para cortar: ').lower()
+        if otro == 'salir':
+            return lista
 
 if __name__ == "__main__":
     menu()
-    print()
 
     #Inputs:
     opcion = int(input("Ingresa la opción aquí: "))
-    numeros_str = input("Ingresa los números a operar: ")
-    lista_numeros = convertir_numero(numeros_str)
+    listado_numeros = lista_numeros()
 
     #Instancia y condicional para seguir operando la calculadora
-    calculator = Calculadora(*lista_numeros)
+    calculator = Calculadora(*listado_numeros)
     seguir_operando = True
 
     while(seguir_operando):
